245a134f9bc16f7d0bf9c941b1d34a7c3d226405
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-03-09 16:13:05 +0200

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,32 +2,72 @@ const { default: iziToast } = require("izitoast");
 
 const button = document.querySelector("button");
 const inputSearch = document.querySelector(".search");
-const form = document.querySelector("form");
+const imagesGallery = document.querySelector(".gallery");
 
-const options = {
-	method: "GET"
-};
 
-const submitSearchImages = button.addEventListener("submit", function(e){
-  console.log(inputSearch.value,  form.event.target.elements.searchImages.value);
-  fetch("https://pixabay.com/api/?key=42766573-a347fa67a5b7233d1286bfaa7&q=inputSearch.value_type=photo")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    iziToast.success {
-      title: "",
-      message: "Sorry, there are no images matching your search query. Please try again!"
-    }
-    // Data handling
-		console.log(data);
-  })
-  .catch(error => {
-    // Error handling
-		console.log(error);
-  });
-})
+const submitSearchImages = button.addEventListener("submit", function(){
+  fetchImages()
+     .then((images) => renderImages(images))
+     .catch((error) => console.log(error));
+});
 
+function fetchUsers() {
+
+    return 
+    fetch("https://pixabay.com/api/?key=42766573-a347fa67a5b7233d1286bfaa7&q=inputSearch.value_type=photo"
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+        // iziToast.success {
+        //   title: "",
+        //   message: "Sorry, there are no images matching your search query. Please try again!"
+        // }
+      }
+      return response.json();
+    });
+  }
+
+ 
+
+// const fetchUsersBtn = document.querySelector(".btn");
+// const userList = document.querySelector(".user-list");
+
+// fetchUsersBtn.addEventListener("click", () => {
+//   fetchUsers()
+//     .then((users) => renderUsers(users))
+//     .catch((error) => console.log(error));
+// });
+
+// function fetchUsers() {
+//   return fetch(
+//     "https://jsonplaceholder.typicode.com/users?_limit=7&_sort=name"
+//   ).then((response) => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+
+function renderUsers(images) {
+  const markup = images
+    .map((image) => {
+      return 
+      `<a class="gallery-link" href="${original}">
+  
+      <img
+
+         class="gallery-image"
+
+         src="${preview}"
+
+         alt="${description}"
+
+       />
+
+     </a>`;
+      
+    })
+    .join("");
+  imagesGallery.insertAdjacentHTML("beforeend", markup);
+  }
--- src/main.js
+++ src/main.js
@@ -9,29 +9,87 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const button = document.querySelector("button");
 const inputSearch = document.querySelector(".search");
-const form = document.querySelector("form");
+const imagesGallery = document.querySelector(".gallery");
 
-const options = {
-	method: "GET"
-};
 
-const submitSearchImages = button.addEventListener("submit", function(e){
-  console.log(inputSearch.value,  form.event.target.elements.searchImages.value);
-  if(inputSearch.value) {
-  fetch("<https://jsonplaceholder.typicode.com/inputSearch.value>")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Data handling
-		console.log(data);
-  })
-  .catch(error => {
-    // Error handling
-		console.log(error);
-  });
+// const submitSearchImages = button.addEventListener("submit", function(){
+//   fetchImages()
+//      .then((images) => renderImages(images))
+//      .catch((error) => console.log(error));
+// });
+
+// function fetchUsers() {
+
+//     return 
+//     fetch("https://pixabay.com/api/?key=42766573-a347fa67a5b7233d1286bfaa7&q=inputSearch.value_type=photo"
+//     ).then((response) => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//         // iziToast.success {
+//         //   title: "",
+//         //   message: "Sorry, there are no images matching your search query. Please try again!"
+//         // }
+//       }
+//       return response.json();
+//     });
+//   }
+
+ 
+// function renderUsers(images) {
+//   const markup = images
+//     .map((image) => {
+//       return 
+//       `<a class="gallery-link" href="${original}">
+  
+//       <img
+
+//          class="gallery-image"
+
+//          src="${preview}"
+
+//          alt="${description}"
+
+//        />
+
+//      </a>`;
+      
+//     })
+//     .join("");
+//   imagesGallery.insertAdjacentHTML("beforeend", markup);
+//   }
+
+button.addEventListener("click", function(event) {
+  event.preventDefault(); // Відміна перезавантаження сторінки під час події click на кнопці
+  fetchImages()
+    .then(images => renderImages(images))
+    .catch(error => console.log(error));
+});
+
+function fetchImages() {
+  const url = "https://pixabay.com/api/?key=42766573-a347fa67a5b7233d1286bfaa7&q=${inputSearch.value}&image_type=photo";
+  
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
 }
-})
+
+function renderImages(images) {
+  // const markup = images.map(image => {
+  //   return 
+  //      <a class="gallery-link"
+  //     // href="${image.largeImageURL}">
+  //     //   <img
+  //     //      class="gallery-image"
+  //     //      src="${image.webformatURL}"
+  //     //      alt="${image.tags}"
+  //        >
+  //     </a>;
+  // }).join("");
+
+  // imagesGallery.innerHTML = markup;
+  console.log('2');
+}
