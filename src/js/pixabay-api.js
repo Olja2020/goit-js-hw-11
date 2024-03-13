c176d9b32edaee4161cebe80965d323879475cea
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-03-13 15:41:36 +0200

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,8 @@
 const { default: iziToast } = require("izitoast");
 
+const button = document.querySelector("button");
+const inputSearch = document.querySelector(".search");
+
  export function fetchImages() {
   let inputSearchValue = inputSearch.value;
   const url = `https://pixabay.com/api/?key=42766573-a347fa67a5b7233d1286bfaa7&q=${inputSearchValue}&image_type=photo&orientation=horizontal&safesearch=true`;
