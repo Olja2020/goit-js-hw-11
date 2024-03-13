9aca36856f6d25691eabbac4df1687ca7fe47cdc
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-03-13 22:06:34 +0200

    add

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { renderUsers } from './js/render-functions';
-//import { fetchImages } from './js/pixabay-api';
+import { fetchImages } from './js/pixabay-api';
 
 const button = document.querySelector('button');
 const inputSearch = document.querySelector('.search');
@@ -20,46 +20,6 @@ const submitSearchImages = form.addEventListener('submit', function (e) {
   fetchImages();
 });
 
-function fetchImages() {
-  let inputSearchValue = inputSearch.value;
-  const url = `https://pixabay.com/api/?key=42766573-a347fa67a5b7233d1286bfaa7&q=${inputSearchValue}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-  if (inputSearchValue) {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-
-      .then(data => {
-        if (data.hits.length === 0) {
-          iziToast.error({
-            title: '',
-            message:
-              'Sorry, there are no images matching your search query. Please try again!',
-          });
-        } else {
-          const imagesData = {
-            totalHits: data.totalHits,
-            total: data.total,
-            images: data.hits,
-          };
-          renderUsers(data.hits);
-        }
-      })
-
-      .catch(error => {
-        console.log(error);
-        iziToast.error({
-          title: '',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-      });
-  }
-}
 const gallery = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captionsData: 'alt',
