6d10b53b235e281b1b803467095ff5a3881865c1
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-03-14 16:13:12 +0200

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -7,6 +7,9 @@ const inputSearch = document.querySelector('.search');
 
 import { renderUsers } from './render-functions';
 
+const hideLoader = () => {
+  loader.style.display = 'none';
+};
 export function fetchImages() {
   let inputSearchValue = inputSearch.value;
   const url = `https://pixabay.com/api/?key=42766573-a347fa67a5b7233d1286bfaa7&q=${inputSearchValue}&image_type=photo&orientation=horizontal&safesearch=true`;
@@ -34,6 +37,7 @@ export function fetchImages() {
             images: data.hits,
           };
           renderUsers(data.hits);
+          hideLoader();
         }
       })
 
