e1cb21c7b284d259baca7e048863eb1fbcc14c1a
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-03-15 17:16:52 +0200

    add

--- src/main.js
+++ src/main.js
@@ -7,4 +7,32 @@ import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+//import { renderUsers } from './js/render-functions';
+import { fetchImages } from './js/pixabay-api';
 
+const button = document.querySelector('button');
+const inputSearch = document.querySelector('.search');
+const imagesGallery = document.querySelector('.gallery');
+const form = document.querySelector('.form');
+const loader = document.querySelector('.loader');
+
+debugger
+const gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+// gallery.refresh();
+// const hideLoader = () => {
+//   loader.style.display = 'none';
+// };
+
+const submitSearchImages = form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  debugger
+  loader.style.display = 'flex';
+  imagesGallery.innerHTML = '';
+  fetchImages();
+  loader.style.display = 'none';
+  form.reset();
+});
