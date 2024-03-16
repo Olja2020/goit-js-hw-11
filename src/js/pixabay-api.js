b1676f5ad296b2c819b6a921bbc7735f5a46772f
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-03-16 17:17:47 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -12,6 +12,7 @@ debugger
 const hideLoader = () => {
   loader.style.display = 'none';
 };
+hideLoader();
 export function fetchImages() {
   let inputSearchValue = inputSearch.value;
   const url = `https://pixabay.com/api/?key=42766573-a347fa67a5b7233d1286bfaa7&q=${inputSearchValue}&image_type=photo&orientation=horizontal&safesearch=true`;
