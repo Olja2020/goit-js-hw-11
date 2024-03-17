d1453af989c826f4010d00b9bd2dda2c389210a2
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2024-03-17 18:03:26 +0200

    add

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -13,36 +13,38 @@ export function fetchImages() {
   const url = `https://pixabay.com/api/?key=42766573-a347fa67a5b7233d1286bfaa7&q=${inputSearchValue}&image_type=photo&orientation=horizontal&safesearch=true`;
 
   if (inputSearchValue) {
-    fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
+    setTimeout(() => {
+      fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(response.status);
+          }
+          return response.json();
+        })
 
-      .then(data => {
-        if (data.hits.length === 0) {
-          iziToast.error({
-            title: '',
-            message:
-              'Sorry, there are no images matching your search query. Please try again!',
-          });
-        } else {
-          const imagesData = {
-            totalHits: data.totalHits,
-            total: data.total,
-            images: data.hits,
-          };
-          renderUsers(data.hits);
-        }
-      })
+        .then(data => {
+          if (data.hits.length === 0) {
+            iziToast.error({
+              title: '',
+              message:
+                'Sorry, there are no images matching your search query. Please try again!',
+            });
+          } else {
+            const imagesData = {
+              totalHits: data.totalHits,
+              total: data.total,
+              images: data.hits,
+            };
+            renderUsers(data.hits);
+          }
+        })
 
-      .catch(error => {
-        console.log(error);
-      })
-      .finally(() => {
-        loader.style.display = 'none';
-      });
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(() => {
+          loader.style.display = 'none';
+        });
+    }, 1000);
   }
 }
